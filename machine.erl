fa064ac787526ad99e42690f55b35ae7a392b714
suchy1713 <wawrzynow.piotr@gmail.com>
2020-01-11 19:57:23 +0100

    wash and finishing touches

--- machine.erl
+++ machine.erl
@@ -33,8 +33,6 @@ prewash(PPID) ->
     LoopPID!{"Start"},
     receive
         {"Prewash_Success"} -> 
-            % print({printstep, "koniec"}),
-            % spawn(machine, wash, [PPID,30,100])
             PPID!{"Prewash_Success"}
     end.
 
@@ -57,10 +55,67 @@ prewash_receive(PPID) ->
     end.
 
 wash(PPID, T, RPM) ->
-    print({printstate, "KKK"}).
+    print({printstate, "WASHING"}),
+    LoopPID = spawn(machine, wash_receive, [self(), T, RPM]),
+    LoopPID!{"Start"},
+    receive
+        {"Wash_Success"} -> 
+            PPID!{"Wash_Success"}
+    end.
+
+wash_receive(PPID, T, RPM) -> 
+    receive
+        {"Start"} -> 
+            spawn(machine, water_fill, [self()]),
+            wash_receive(PPID, T, RPM);
+        {"Filled"} -> 
+            spawn(machine, heat, [self(), T]),
+            wash_receive(PPID, T, RPM);
+        {"Heated"} -> 
+            spawn(machine, rotate, [self(), 400, 60]),
+            wash_receive(PPID, T, RPM);
+        {"Rotated"} ->
+            spawn(machine, cold_water_fill, [self()]),
+            wash_receive(PPID, T, RPM);
+        {"Filled_Cold"} ->
+            spawn(machine, rotate2, [self(), 400, 15]),
+            wash_receive(PPID, T, RPM);
+        {"Rotated2"} ->
+            PPID!{"Wash_Success"}
+    end.
+
+touches(PPID, T, RPM) ->
+    print({printstate, "FINISHING TOUCHES"}),
+    LoopPID = spawn(machine, touches_receive, [self(), T, RPM]),
+    LoopPID!{"Start"},
+    receive
+        {"FT_Success"} -> 
+            PPID!{"FT_Success"}
+    end.
+
+touches_receive(PPID, T, RPM) -> 
+    receive
+        {"Start"} -> 
+            spawn(machine, cold_water_fill, [self()]),
+            touches_receive(PPID, T, RPM);
+        {"Filled_Cold"} -> 
+            spawn(machine, get_zmiekczacz, [self()]),
+            touches_receive(PPID, T, RPM);
+        {"Filled_Zmiekczacz"} -> 
+            spawn(machine, rotate, [self(), 400, 10]),
+            touches_receive(PPID, T, RPM);
+        {"Rotated"} ->
+            spawn(machine, pump, [self()]),
+            touches_receive(PPID, T, RPM);
+        {"Pumped"} ->
+            spawn(machine, rotate_fast, [self(), 400, 15]),
+            touches_receive(PPID, T, RPM);
+        {"Rotated_Fast"} ->
+            PPID!{"FT_Success"}
+    end.
 
 pump(PPID) ->
-    print({printstep, "Pumping water"}),
+    print({printstep, "Pumping water out"}),
     print({in_progress}),
     mySleep(40),
     print({finished}),
@@ -73,19 +128,47 @@ rotate(PPID, RPM, Time) ->
     print({finished}),
     PPID!{"Rotated"}.
 
+rotate2(PPID, RPM, Time) ->
+    print({printstep, "Rotating at " ++ integer_to_list(RPM) ++ " RPM"}),
+    print({in_progress}),
+    mySleep(Time),
+    print({finished}),
+    PPID!{"Rotated2"}.
+
+rotate_fast(PPID, RPM, Time) ->
+    print({printstep, "Rotating at " ++ integer_to_list(5*RPM) ++ " RPM"}),
+    print({in_progress}),
+    mySleep(Time),
+    print({finished}),
+    PPID!{"Rotated_Fast"}.
+
 heat(PPID, T) ->
     print({printstep, "Heating water up to " ++ integer_to_list(T) ++ " C"}),
     print({in_progress}),
-    mySleep(T*5),
+    mySleep(T/2),
     print({finished}),
     PPID!{"Heated"}.
 
 water_fill(PPID) ->
     print({printstep, "Filling with water"}),
     print({in_progress}),
-    mySleep(60),
+    mySleep(10),
     print({finished}),
     PPID!{"Filled"}.
+
+cold_water_fill(PPID) ->
+    print({printstep, "Filling with cold water"}),
+    print({in_progress}),
+    mySleep(10),
+    print({finished}),
+    PPID!{"Filled_Cold"}.
+
+get_zmiekczacz(PPID) ->
+    print({printstep, "Filling with zmiekczacz do tkanin"}),
+    print({in_progress}),
+    mySleep(2),
+    print({finished}),
+    PPID!{"Filled_Zmiekczacz"}. 
 
 main_receive(PPID, T, RPM, Should_Prewash) ->
     receive
@@ -99,7 +182,10 @@ main_receive(PPID, T, RPM, Should_Prewash) ->
             spawn(machine, wash, [self(), T, RPM]),
             main_receive(PPID, T, RPM, Should_Prewash);
         {"Wash_Success"} -> 
-            -1 % tu sie wykaz
+            spawn(machine, touches, [self(), T, RPM]),
+            main_receive(PPID, T, RPM, Should_Prewash);
+        {"FT_Success"} ->
+            PPID!{"Finish"}
     end.
 
 start(T, RPM, Should_Prewash) ->
@@ -107,5 +193,7 @@ start(T, RPM, Should_Prewash) ->
     ReceivePID = spawn(machine, main_receive, [self(), T, RPM, Should_Prewash]),
     spawn(machine, lock, [ReceivePID]),
     receive
-        {"Finish"} -> 0 % na koniec przesyłamy sobie finisz
+        {"Finish"} -> print({printstate, "FINISHED"}),
+                       print({printstep, ""}) % na koniec przesyłamy sobie finisz
     end.
+    
